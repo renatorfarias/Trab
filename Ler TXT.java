3ca79ce74a4ee08f8750ffc3110d1b60c153b651
MasaakiNakamura <masaaki_naka@hotmail.com>
2016-12-17 20:49:04 -0300

    Update

--- Ler TXT.java
+++ Ler TXT.java
@@ -5,6 +5,11 @@ import java.io.*;
 public class LerTxt {
 	public void LerTXT(){
 		CriarTxt nome = new CriarTxt();
+		int a = 0; //parametro apra mudar a leitura de usuario e senha
+		int k = 0;
+		String loginUsuario[] = new String[5];
+		String loginSenha[] = new String[5];
+		String dados = "";
 		String mostrar = ""; //Local para armazenar o conteudo do arquivo
 		String nomeDoArq = nome.nomeDoArquivo; // nome do do arquivo (eu importei da classe 'CriarTxt' para manter o mesmo nome)
 		String linha = "";
@@ -12,26 +17,35 @@ public class LerTxt {
 		if(arquivo.exists()){ //retorna valor verdadeiro ou falso
 			mostrar = "Arquivo: " + nomeDoArq + "\n";
 			try{
-				mostrar += "Conteúdo: \n";
+				mostrar += "Dados:\n";
 				//abrindo arquivo para leitura
 				FileReader reader = new FileReader(nomeDoArq);
-				//leitor do arquivo	
+				//leitor do arquivo
 				BufferedReader leitor = new BufferedReader(reader);
 				while(true){
+					loginUsuario[a] = leitor.readLine();
+					loginSenha[a] = leitor.readLine();
 					linha = leitor.readLine();
-					if(linha == null){
+					if((linha == null) || (loginUsuario[a] == null) || (loginSenha[a] == null)){
 						break;
 					}
-				mostrar += linha + "\n";
+					a++;
+				mostrar += linha;
 				}
 			}
 			catch (Exception erro) {}
-			JOptionPane.showMessageDialog(null, mostrar, nomeDoArq, 1);
+			/*
+			for(int i = 0; i < 5; i++){
+				loginUsuario[a] = 0;
+				loginSenha[a] = 0; 
+			}
+			*/
+			for(int i = 0; i <= a; i++){
+				dados += loginUsuario[a] + "\n" + loginSenha[a] + "\n";
+			}
+			JOptionPane.showMessageDialog(null, mostrar + "\n" + dados, nomeDoArq, 1);
 		} else {
 			JOptionPane.showMessageDialog(null, "Arquivo não existe", "Erro", 0);
 		}
 	}
 }
-
-	
-
