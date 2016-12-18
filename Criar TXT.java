fa7862bf70567b3e12b3e62949a40408a0e9ba3c
MasaakiNakamura <masaaki_naka@hotmail.com>
2016-12-18 00:20:39 -0300

    Update Criar TXT.java

--- Criar TXT.java
+++ Criar TXT.java
@@ -7,7 +7,9 @@ public class CriarTxt {
 	String nomeDeUsuario[] = new String [5];
 	String senha[] = new String[5];
 	String informacoes ="";
+	String login[][] = new String[5][2];
 	int i = 0;
+	int a = 0;
 	boolean control = false;
 	
 	public void CriarTXT(){
@@ -16,13 +18,13 @@ public class CriarTxt {
 			JOptionPane.showMessageDialog(null, "Digite os dados do seu cadastro:");
 			this.nomeDeUsuario[i] = JOptionPane.showInputDialog("Nome de usuário:");
 			this.senha[i] = JOptionPane.showInputDialog("Senha:");
-			//informacoes += "usuario[" + i + "]" + " = {" + this.nomeDeUsuario[i] + ", " + this.senha[i] + "}; \n";
-			informacoes += this.nomeDeUsuario[i] + "\n" + this.senha[i] + "\n";
+			login[a][0] = nomeDeUsuario[a];
+			login[a][1] = senha[a];
+			informacoes += this.nomeDeUsuario[i] + "\n" + this.senha[i] + "\n\n";
 			saida.format(informacoes);
-			i++;
+			a++;
 			saida.close();
 			JOptionPane.showMessageDialog(null,"Arquivo " + this.nomeDoArquivo + " criado!", "Arquivo",1 );
-			control = true;
 		}
 		catch (Exception erro){
 			JOptionPane.showMessageDialog(null, "Arquivo não põde ser gerado");
